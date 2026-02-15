11113eb85af7ef6369ed3cf79445ff0cff8ae178
Ashish Jadhav <140818679+jadhav87ashish@users.noreply.github.com>
2026-02-15 23:09:14 +0530

    Create Tut05_ControlStatements.java

---
+++ src/Tut05_ControlStatements.java
@@ -0,0 +1,26 @@
+public class Tut05_ControlStatements {
+    public static void main(String[] args) {
+        //conditional
+        // if
+
+        int age = 25;
+        if (age>18)
+        {
+            System.out.println("Eligible for vote");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
