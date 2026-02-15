bffce45a3b3e48e29a6477996f4608401084054b
Ashish Jadhav <140818679+jadhav87ashish@users.noreply.github.com>
2026-02-15 21:05:27 +0530

    Create Tut03_DataTypes.java

---
+++ src/Tut03_DataTypes.java
@@ -0,0 +1,28 @@
+public class Tut03_DataTypes {
+    public static void main(String[] args) {
+        //Numerical
+        int a = 100;
+        int b = 200;
+        System.out.println(a+b);
+
+        byte d = 125;
+
+        short e = 3535;
+        long l = 212121212121L;
+
+
+        // Decimal
+
+        float f = 1524.5555F;
+        double doub = 454541515.4564746;
+
+        // Character
+        char x = 'A';
+
+
+        // Boolean
+        boolean i = true;
+        boolean y = false;
+
+    }
+}
