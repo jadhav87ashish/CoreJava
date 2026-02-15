392cefafef35f25af6c485a66c7bccbb824fd93f
Ashish Jadhav <140818679+jadhav87ashish@users.noreply.github.com>
2026-02-15 20:12:47 +0530

    first commit

---
+++ src/Tut001.java
@@ -0,0 +1,39 @@
+public class Tut001 {
+    public static void main(String[] args) {
+        // byte
+        System.out.println("byte");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+        // short
+        System.out.println("short");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+        // int
+        System.out.println("int");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+
+        // long
+        System.out.println("long");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        // float
+        System.out.println("float");
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+        // double
+        System.out.println("double");
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+        // char
+        System.out.println("char");
+        System.out.println((int)Character.MIN_VALUE);
+        System.out.println((int)Character.MAX_VALUE);
+
+    }
+}
