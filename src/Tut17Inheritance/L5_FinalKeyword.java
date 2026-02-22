20342cb53ae0a11ed2abd533163684c75d8be90d
Ashish Jadhav <140818679+jadhav87ashish@users.noreply.github.com>
2026-02-22 11:28:24 +0530

    final keyword

---
+++ src/Tut17Inheritance/L5_FinalKeyword.java
@@ -0,0 +1,36 @@
+package Tut17Inheritance;
+
+//final keyword
+//when applies to;
+//varible, its value can not be changes
+//method, it can not be override
+//class, it can not be inherited/extended to child
+
+class Test
+{
+//    int x= 100;
+    final int x = 100;
+}
+
+final class Test1 {
+    final void m(){
+        System.out.println("test 1 method: method called from test1 class");
+    }
+}
+
+class Test2 extends Test1{  // final class
+    void m(){
+        System.out.println("test 1 method: method called from test2 class");// final method
+    }
+}
+
+
+public class L5_FinalKeyword {
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.x = 200; // x is final variable can not be changed
+        System.out.println(t.x);
+
+    }
+
+}
